71e9ac3b809df03b0cf51cc9125758271b317fef
tianyong <tianyong@qianxin.com>
2022-06-23 09:46:39 +0800

    fangdongzhizu init

---
+++ src/test/java/com/data/fangdongzhizu/FangdongzhizuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.fangdongzhizu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FangdongzhizuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
