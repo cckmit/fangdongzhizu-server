6f35aac80e3fa2e44209a32bbe8346ecc6bcfc89
tianyong <tianyong@qianxin.com>
2022-08-01 18:11:03 +0800

    feat: 格式化代码

--- user-server/src/main/java/com/data/userserver/UpmApplication.java
+++ user-server/src/main/java/com/data/userserver/UpmApplication.java
@@ -1,10 +1,13 @@
 package com.data.userserver;
 
+import com.data.signal.impl.ChatServer;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan("com.data.userserver.mapper")
 @ComponentScan(basePackages = "com.data")
@@ -14,6 +17,20 @@ public class UpmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UpmApplication.class, args);
+
+        // 启用聊天模块
+        final ChatServer server = new ChatServer();
+        server.init();
+        server.start();
+        // 注册进程钩子，在JVM进程关闭前释放资源
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run(){
+                server.shutdown();
+                log.warn("----------服务器清理资源完毕，服务正在关闭中ing----------");
+                System.exit(0);
+            }
+        });
     }
 
 }
